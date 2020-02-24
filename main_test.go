7c139cd49fd8c395b558701a3b2c3dbfca0de76c
Wojciech Kaczmarek <wojtekk@kofeina.net>
2020-02-24 13:42:10 +0100

    main test via pipe/TeeReader

---
+++ main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func ExampleMain() {
+	os.Args = os.Args[:1]
+	os.Args = append(os.Args, "2")
+
+	b := new(bytes.Buffer)
+	b.WriteString("00112233")
+
+	feed(&os.Stdin, b)
+	main()
+
+	// Output:
+	// 00 11 22 33
+}
+
+func feed(fp **os.File, b *bytes.Buffer) {
+	pr, pw, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	*fp = pr
+	ioutil.ReadAll(io.TeeReader(b, pw))
+	pw.Close()
+}
