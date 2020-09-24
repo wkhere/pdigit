c42442100b9a79d0d33642ceefa579fd3b6d1248
Wojciech Kaczmarek <wojtekk@kofeina.net>
2020-09-24 13:26:00 +0200

    main test (pipe/TeeReader): simplify feed func sig

--- main_test.go
+++ main_test.go
@@ -21,7 +21,7 @@ func ExampleMain() {
 	// 00 11 22 33
 }
 
-func feed(fp **os.File, b *bytes.Buffer) {
+func feed(fp **os.File, b io.Reader) {
 	pr, pw, err := os.Pipe()
 	if err != nil {
 		panic(err)
